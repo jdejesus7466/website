85ad514296bf229076667fb8bb2c6e23a9beb893
Joseph De Jesus <jdejesus7466@gmail.com>
2024-07-18 19:25:32 -0700

    Adding Resume Button to Hero

--- src/Components/Hero/hero.jsx
+++ src/Components/Hero/hero.jsx
@@ -9,12 +9,17 @@ const hero = () => {
     document.getElementById('about-me').scrollIntoView({ behavior: 'smooth' });
   };
 
+  const handleResumeClick = () => {
+    window.open('https://docs.google.com/document/d/14KCJIz-koIY2G0MHyioicH-Be_7mgoY7Er3cOgqGrbI/edit?usp=sharing', '_blank');
+  };
+
   return (
     <div className='hero container'>
         <div className="heroText">
             <h1>Welcome to my Website!</h1>
             <p>Information and Pieces of Code from Joseph De Jesus</p>
             <button onClick={handleScroll} className='btn'>See More!<img src={heroImg} alt="" /></button>
+            <button onClick={handleResumeClick} className='btn'>Resume</button>
         </div>
     </div>
   )
