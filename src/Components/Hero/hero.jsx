37eef91e356d88916dcbc58cdb67477c23fe89ad
Joseph De Jesus <jdejesus7466@gmail.com>
2024-09-19 21:52:14 -0700

    Overall Design Changes to multiple aspects

--- src/Components/Hero/hero.jsx
+++ src/Components/Hero/hero.jsx
@@ -7,12 +7,23 @@ const hero = () => {
     window.open('https://docs.google.com/document/d/1PT4ks29Q0zxmWw4plIZWjVBmGPWbZz73yMvoLbmRbsU/edit?usp=sharing', '_blank');
   };
 
+  const handleLinkedIn = () => {
+    window.open('https://www.linkedin.com/in/joseph-de-jesus-91880b264/', '_blank');
+  };
+
+  const handleGitHub = () => {
+    window.open('https://github.com/jdejesus7466', '_blank');
+  };
+  
+
   return (
     <div className='hero container'>
         <div className="heroText">
             <h1>Welcome to my Website!</h1>
             <p>Information and Pieces of Code from Joseph De Jesus</p>
             <button onClick={handleResumeClick} className='btn'>Resume</button>
+            <button onClick={handleLinkedIn} className='btn'>LinkedIn</button>
+            <button onClick={handleGitHub} className='btn'>GitHub</button>
         </div>
     </div>
   )
--- src/Components/programs/programs.jsx
+++ src/Components/programs/programs.jsx
@@ -6,15 +6,30 @@ import programImg from '../../assets/logo.png';
 const Programs = () => {
   return (
     <div className='programs'>
-        <h1>Programs that I have made or contributed to.</h1>
-        <div className="program">
-            <p className='explanation'>Left</p>
-            <img src={practice} alt="Program 1" />
+      <h1>Programs that I have made or contributed to.</h1>
+
+      <div className="program">
+        <div className="program-info">
+          <h2>Swim Practice Maker</h2>
+          <p className='explanation'>As a swim coach, it can be difficult to come up with practices while also visualizing the
+            total distance and workouts. This website is programmed to make swim practices while also listing it down, and tracking
+            the total distance of the sets.
+          </p>
         </div>
-        <div className="program">
-            <img src={programImg} alt="Program 2" />
-            <p className='explanation'>Right</p>
+        <a href="https://jdejesus7466.github.io/practice-designer/" target="_blank" rel="noopener noreferrer">
+          <img src={practice} alt="Program 1" />
+        </a>
+      </div>
+
+      <div className="program">
+        <a href="https://example2.com" target="_blank" rel="noopener noreferrer">
+          <img src={programImg} alt="Program 2" />
+        </a>
+        <div className="program-info">
+          <h2>Weather App</h2>
+          <p className='explanation'>A simple weather app that tracks the weather based on an inputted city.</p>
         </div>
+      </div>
     </div>
   );
 }
